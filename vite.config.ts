e488fcc1112159c187dbdd438a167c6094d76032
coderIndu <70372049+coderIndu@users.noreply.github.com>
2023-03-25 18:37:52 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import viteCompression from 'vite-plugin-compression'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin(), viteCompression()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, 'src'),
       'antd/dist/reset.css': path.join(__dirname, 'node_modules/antd/dist/reset.css'),  // <-- add this
-      antd: path.join(__dirname, 'node_modules/antd/dist/antd.js'),
-      '@ant-design/icons': path.join(__dirname, 'node_modules/@ant-design/icons/dist/index.umd.js'),
+      "antd": path.join(__dirname, 'node_modules/antd/dist/antd.js'),
+      '@ant-design/icons': path.join(__dirname, 'node_modules\\@ant-design\\icons\\lib\\index.js'),
+      'react-syntax-highlighter/dist/cjs/styles/prism': path.join(__dirname, 'node_modules/react-syntax-highlighter/dist/cjs/styles/prism/index.js'),
+      'react-syntax-highlighter': path.join(__dirname, 'node_modules/react-syntax-highlighter/dist/cjs/index.js'),
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      cache: true,
+      treeshake: true,
     }
   }
 })
